e789f2e8f3f830e0a29f507c5a7c0caf9e380a87
Ramin Sultangaliev <raminsultan07@mail.ru>
2023-08-31 14:10:04 +0400

    added dark theme and theme switcher

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const themeButton = document.querySelector('.theme-button')
+
+if (localStorage.getItem('theme') == 'dark') {
+    themeButton.classList.add('theme-button_active')
+    document.body.classList.add('dark')
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    themeButton.classList.add('theme-button_active')
+    document.body.classList.add('dark')
+}
+
+themeButton.onclick = () => {
+    let isDark = document.body.classList.toggle('dark')
+    themeButton.classList.toggle('theme-button_active')
+    if (isDark) {
+        localStorage.setItem('theme', 'dark')
+    } else {
+        localStorage.setItem('theme', 'light')
+    }
+}
